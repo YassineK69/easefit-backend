f1627ec828b677745e6651cda6406c5a8aa1a6d0
Dal ferro Christian <christiandalferro@gmail.com>
2025-08-05 11:37:31 +0200

    avant Fanny

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const { checkBody } = require("../modules/checkBody");
 const bcrypt = require("bcrypt");
 const uid2 = require("uid2");
+const User = require('../models/users'); 
+
 
 /* signIn */
 
@@ -11,34 +13,47 @@ const uid2 = require("uid2");
 
 
 /* signUp */
-router.post('/signup', (req, res,) => {
-  if(!checkBody(req.body, ['email','password', 'lastName' , 'firstName' , 'gender' , 'birthday' , 'hight'])) {
-     res.json({result: false, error: 'Missing or empty fields'});
-     return;  
-  };
- 
-  User.findOne({ username: req.body.username}).then(data => {
-    if(data === null){
+router.post("/signup", (req, res) => {
+  console.log('route reçue',req.body)
+  if (
+    !checkBody(req.body, [
+      "email",
+      "password",
+      "lastName",
+      "firstName",
+      "gender",
+      "birthday",
+      "height",
+    ])
+  ) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  User.findOne({ email: req.body.email }).then((data) => {
+    if (data === null) {
       const hash = bcrypt.hashSync(req.body.password, 10);
-      const newUser = new User ({
-        name: req.body.name,
-        username: req.body.username,
+      const newUser = new User({
+        email: req.body.email,
         password: hash,
         token: uid2(32),
+        lastName: req.body.lastName,
+        firstName: req.body.firstName,
+        gender: req.body.gender,
+        birthday: req.body.birthday,
+        height: req.body.height,
+        profilePic: 'avatar.png',
+        idActivities: [],
       });
 
       newUser.save().then((newDoc) => {
-        res.json({result: true, token: newDoc.token});
+        res.json({ result: true, token: newDoc.token });
       });
-    }else{
+    } else {
       // User already exist in database
-      res.json ({ result: false, error: 'User already exists'})
+      res.json({ result: false, error: "User already exists" });
     }
   });
 });
-
-
-
-
 
 module.exports = router;
