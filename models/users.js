80f5af40c60130771de52d2c1e5647fed972022b
Your name <ludmilarose1019@gmail.com>
2025-08-05 15:13:50 +0200

    création route profil

--- models/users.js
+++ models/users.js
@@ -19,7 +19,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-
 const User = mongoose.model('User', userSchema);
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/users');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* Get user infos. */
+router.get('/user/:token', async (req, res) => {
+  const userToken = req.params.token; //revoir
+
+  try {
+if (!userToken) {
+    return res.status(401).json({ message: 'Token non trouvé' });
+    }
+
+  const user = await User.findOne({ token: req.params.token }).populate('Activity');
+
+if (!user) {
+    return res.status(404).json({ message: 'Utilisateur non trouvé'});
+}
+
+  const userInfo = {
+      prenom: user.prenom,
+      dateDeNaissance: user.dateDeNaissance,
+      genre: user.genre,
+      taille: user.taille,
+      nombreActivites: user.activities ?
+      user.activites.length: 0,
+    };
+
+  res.json(userInfo);
+
+  }catch (error) { 
+    console.error('Erreur lors de la récupération des données du user :', error);
+    res.status(500).json({ message: 'Erreur serveur'});
+  }
 });
 
 module.exports = router;
